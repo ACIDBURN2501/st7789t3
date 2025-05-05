e393887d72e3416cf69a367fc6eb5ffb5e6c5715
Aaron <ajl@magnusmarine.com>
2025-05-05 09:40:03 +0100

    Updated tests to reflect module changes

--- tests/test_display.c
+++ tests/test_display.c
@@ -1,11 +1,28 @@
 #include "unity.h"
-#include "display_init.h"
 
-void setUp(void) {}
-void tearDown(void) {}
+#include "image_loader.h"
 
-void test_image_loader_returns_error_with_null_buffer(void)
+void
+setUp(void)
 {
-    int result = load_image_as_rgb565("assets/splash.png", NULL, 240, 320);
-    TEST_ASSERT_EQUAL_INT_MESSAGE(-1, result, "Expected error for NULL buffer");
+}
+void
+tearDown(void)
+{
+}
+
+void
+test_image_loader_returns_error_with_null_buffer(void)
+{
+        int result = image_load_rgb565("assets/splash.png", NULL, 240, 320);
+        TEST_ASSERT_EQUAL_INT_MESSAGE(-1, result,
+                                      "Expected error for NULL buffer");
+}
+
+int
+main(void)
+{
+        UNITY_BEGIN();
+        RUN_TEST(test_image_loader_returns_error_with_null_buffer);
+        return UNITY_END();
 }
